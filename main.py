15ae78d48ffc41648470a38bf9b9a7c5350632f2
Sergey_NG <operatorsn@domofon.net>
2017-03-02 16:40:15 +0300

    Get cars pics from SecurOS Data Base splitted by data

--- main.py
+++ main.py
@@ -21,28 +21,84 @@ from PyQt4 import QtSql
 import ConfigParser
 import sys
 import re
-import binascii
+import ftplib
 import os
+
+
+class send_to_FTP (object):
+    def __init__(self):
+        global globalConf
+        self.ftpconf = ftplib.FTP()
+        self.ftpconf.set_pasv(True)
+        self.ftpconf.connect(globalConf.get('FTP', 'FTP'), int(globalConf.get('FTP', 'FTPPort')))
+        self.ftpconf.login(globalConf.get('FTP', 'FTPUser'), globalConf.get('FTP', 'FTPPassword'))
+        #make directory
+        #self.ftpconf.mkd('IVAN')
+        #remove dir
+        #self.ftpconf.rmd('IVAN')
+        #print self.ftpconf.dir()
+        filewherepics = globalConf.get ('options', 'folderpath')
+        #self.uploadThis(os.path.abspath(filewherepics))
+        self.uploadF(os.path.abspath(filewherepics))
+    def uploadF (self, path):
+        files = os.listdir(path)
+        for f in files:
+            if f not in self.ftpconf.nlst():
+                self.ftpconf.mkd(f)
+            for i in os.listdir (path + r'\{}'.format(f)):
+                print i, path + r'\{}'.format(f)+r'\{}'.format(i)
+
+                #print os.listdir(path + r'\{}'.format(f)+r'\{}'.format(i))
+        #print current
+    def uploadThis(self, path):
+        files = os.listdir(path)
+        os.chdir(path)
+        for f in files:
+            if os.path.isfile(path + r'\{}'.format(f)):
+                fh = open(f, 'rb')
+                self.ftpconf.storbinary('STOR %s' % f, fh)
+                fh.close()
+            elif os.path.isdir(path + r'\{}'.format(f)):
+                print self.ftpconf.nlst()
+                if f not in self.ftpconf.nlst():
+                    self.ftpconf.mkd(f)
+                    self.ftpconf.cwd(f)
+                    self.uploadThis(path + r'\{}'.format(f))
+        self.ftpconf.cwd('..')
+        os.chdir('..')
+
 
 # Опции из конфига
 class setoptions (QtGui.QDialog):
     def __init__(self):
         super(setoptions, self).__init__()
         self.setWindowTitle(u'Настройки')
+        self.setMinimumSize(400, 150)
+        self.setMaximumSize(400, 150)
         dialogLayot = QtGui.QVBoxLayout()
+        sec_label = QtGui.QLabel()
+        sec_label.setText(u'Укажите время в СЕКУНДАХ:')
+        dialogLayot.addWidget(sec_label)
         self.setTime = QtGui.QLineEdit()
-        self.setTime.setText(u'Введите через сколько секунд грабить из базы картинки')
+        self.setTime.setText(u'')
+
         horisontalL = QtGui.QHBoxLayout()
         self.filePath = QtGui.QLineEdit()
         self.filePath.setEnabled(False)
         openDialog = QtGui.QPushButton (u'Обзор')
         openDialog.clicked.connect(self.set_dialog)
+
         horisontalL.addWidget(self.filePath)
         horisontalL.addWidget(openDialog)
+
         dialogLayot.addWidget(self.setTime)
+        filez_label = QtGui.QLabel()
+        filez_label.setText(u'Укажите место, где будут располагаться картинки')
+        dialogLayot.addWidget(filez_label)
         dialogLayot.addLayout(horisontalL)
         executeOptions = QtGui.QPushButton (u'Сохранить')
         executeOptions.clicked.connect (self.saveconfig)
+
         dialogLayot.addWidget(executeOptions)
         self.setLayout(dialogLayot)
         self.exec_()
@@ -82,6 +138,7 @@ class setoptions (QtGui.QDialog):
         # Запись в конфиг папки для сохранения
         self.filePath.setText(unicode(show)+'\\')
 
+
 # Класс загрузки картинок в каталоги. Катологизирование: путь из конфига / компьютер / направление / дата время номер.jpg
 class upload (object):
 
@@ -95,7 +152,7 @@ class upload (object):
         query = QtSql.QSqlQuery()
         str1 = 'select time_best,plate_recognized,encode(image,\'hex\'), camera_host, lpr_name from tobackup.t_log s inner join tobackup.t_image m using (tid)'
         query.exec_(str1)
-        count =0
+        count = 0
         flog = ''
         try:
             flog = open('process.log', 'a')
@@ -104,21 +161,28 @@ class upload (object):
             pass
 
         while query.next():
-            file_parth = unicode (globalConf.get('options', 'folderpath')+query.value(3).toString()+'\\'+query.value(4).toString()+'\\'+query.value(0).toDateTime().toString('ddMMyy-hhmmss') + ' ' + query.value(1).toString().replace ('?', '_') + '.jpg')
+            file_parth = unicode(globalConf.get('options', 'folderpath')+query.value(3).toString()+'/'+re.match('[a-zA-Z0-9 ]+(.*)', unicode(query.value(4).toString())).group(1)+'/'+query.value(0).toDateTime().toString('dd_MM_yy')+'/'+query.value(0).toDateTime().toString('dd_MM_yy hh-mm-ss') + ' ' + query.value(1).toString().replace ('?', '_')+'__'+re.match('[a-zA-Z0-9 ]+(.*)', unicode(query.value(4).toString())).group(1)+'.jpg')
             file_parth = file_parth.replace('\\','/')
-            if os.path.exists(str (globalConf.get('options', 'folderpath')+query.value(3).toString())) is False:
-                os.mkdir(str (globalConf.get('options', 'folderpath')+query.value(3).toString()))
-            if  os.path.exists(unicode (globalConf.get('options', 'folderpath')+query.value(3).toString()+'\\'+query.value(4).toString())) is False:
-                os.mkdir(unicode (globalConf.get('options', 'folderpath')+query.value(3).toString()+'\\'+query.value(4).toString()))
+            if os.path.exists(unicode(globalConf.get('options', 'folderpath')+query.value(3).toString())) is False:
+                firstiter = unicode(globalConf.get('options', 'folderpath')+query.value(3).toString())
+                os.mkdir(firstiter)
+            secstep = globalConf.get('options', 'folderpath')+query.value(3).toString()+'/'+re.match('[a-zA-Z0-9 ]+(.*)', unicode(query.value(4).toString())).group(1)+'/'
+            if os.path.exists(unicode(secstep)) is False:
+                os.mkdir(unicode(secstep))
+            tridstep = globalConf.get('options', 'folderpath')+query.value(3).toString()+'/'+re.match('[a-zA-Z0-9 ]+(.*)', unicode(query.value(4).toString())).group(1)+'/'+query.value(0).toDateTime().toString('dd_MM_yy')+'/'
+            if os.path.exists(unicode(tridstep)) is False:
+                os.mkdir(unicode(tridstep))
+
             file = QtCore.QFile(file_parth)
             file.open(QtCore.QIODevice.WriteOnly)
             file.write(str(query.value(2).toString()).decode('hex'))
             file.close()
             print unicode(file_parth)
-            count +=1
+            count += 1
+
         flog.write('Запись завершена в ' + QtCore.QDateTime.currentDateTime().toString('dd-MM-yy hh:mm:ss')+ ' количество файлов ' + str(count)+'\n')
         flog.close()
-        query.exec_('delete from tobackup.t_image;')
+        #query.exec_('delete from tobackup.t_image;')
 
 # проверка на наличие конфиг файла
 def configupload ():
@@ -148,6 +212,7 @@ def main():
     global globalConf
     if reta == True:
         global db
+        #send_to_FTP (app)
         db = QtSql.QSqlDatabase.addDatabase("QPSQL")
         db.setHostName(globalConf.get('database','ip'))
         db.setDatabaseName(globalConf.get('database','database'))
@@ -173,12 +238,15 @@ def main():
         exit = menu.addAction(u'Закрыть')
         exit.triggered.connect(QtGui.qApp.quit)
         tray.setContextMenu(menu)
-        timer = QtCore.QTimer()
-        timer.start(int (globalConf.get('options', 'script_startsec'))*1000)
-        timer.timeout.connect(upload)
+
+        #timer = QtCore.QTimer()
+        #timer.start(int (globalConf.get('options', 'script_startsec'))*1000)
+        #timer.timeout.connect(upload)
+        upload()
         tray.show()
         sys.exit(app.exec_())
     else:
         app.closeAllWindows()
 
 main()
+#send_to_FTP()
